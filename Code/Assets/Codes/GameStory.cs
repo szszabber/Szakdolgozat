88a33cb5efa5af5f6d2b1d3577dfd0d5f21cfacd
Szajduk Szabolcs <71318633+szszabber@users.noreply.github.com>
2020-11-23 16:49:42 +0100

    Show motivations on graph

--- Code/Assets/Codes/GameStory.cs
+++ Code/Assets/Codes/GameStory.cs
@@ -1,7 +1,12 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.IO;
+using System.Xml;
+using System.Xml.Linq;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class GameStory : MonoBehaviour
 {

2eb10f88504a1dab605fc40e6e22c095b655b929
Szajduk Szabolcs <71318633+szszabber@users.noreply.github.com>
2021-01-07 13:08:13 +0100

    Adding MainSceneDataReader

---
+++ Code/Assets/Codes/DataReades/MainSceneDataReader.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Xml.Linq;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MainSceneDataReader : MonoBehaviour
+{
+    public TextAsset xmlRawFile;
+
+    public Text sourceText;
+
+    private void Start()
+    {
+        string data = xmlRawFile.text;
+
+        ReadMainSceneData(data);
+    }
+
+    private void ReadMainSceneData(string xmlFileAsText)
+    {
+        XDocument xmlDoc = XDocument.Parse(xmlFileAsText);
+
+        IEnumerable<XElement> mainScene = xmlDoc.Root.Element("MainSceneData").Elements("MainScene").ToList();
+
+        foreach (XElement mainMenuXelement in mainScene)
+        {
+            string source = mainMenuXelement.Element("Source").Value;
+
+            sourceText.text = source;
+        }
+    }
+}
